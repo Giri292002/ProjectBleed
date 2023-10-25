d823563f8fcf815730b1eb47ea66f529589c2f9a
Giri292002 <pbgiridhara@gmail.com>
2023-10-25 11:52:46 -0700

    Refactoring Weapon
    + Moved weapon information to weapon data
    + Added Different firing methods and handled them
    + Added new data classes to blueprints.
    - Removed Semi Automatic Weapon Class, handled by base itself
    - Remove Automatic Weapon Class, handled by base itself
    
    TODO: Need to create and implement data for Rifle

---
+++ Source/ProjectBleed/Weapons/Data/PBWeaponDataBase.h
@@ -0,0 +1,17 @@
+// All Rights belong to Studio WASD 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PBWeaponDataBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBLEED_API UPBWeaponDataBase : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
